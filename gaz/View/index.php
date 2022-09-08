956b1f593764ccf41bf626e77b8017b8b625d4c8
kristinabutko <krystinabutko@gmail.com>
2022-09-08 14:04:36 +0300

    add gaz/

---
+++ gaz/View/index.php
@@ -0,0 +1,30 @@
+<?php $this->theme->header(); ?>
+
+
+
+<div class="container">
+	<div class='main_page_info'>
+            <h1>Добро пожаловать в ПО "Газовик"</h1>
+			<br>
+			<p>(Блок в проекте).</p>
+			
+		
+			
+
+     </div>
+	<div class='index_menu'>
+		<div class='flex'>
+			<a href='/' class='index_menu_item'> Наш сайт</a>
+			<a href='/ARM/basis/main/' class='index_menu_item'> ПО "БАЗИС"</a>
+			<a href='/ARM/examination/main/' class='index_menu_item'> ПО "Персона"</a>
+		</div>
+		<div class='flex'>
+			<a href='/ARM/ozp/main/' class='index_menu_item'> ОЗП</a>
+			<a href='/ARM/gaz/main/' class='index_menu_item'> Газовик</a>
+			<a href='/phonebook/' class='index_menu_item'> Справочник</a>
+		</div>
+	</div>
+
+</div>
+
+<?php $this->theme->footer(); ?>
